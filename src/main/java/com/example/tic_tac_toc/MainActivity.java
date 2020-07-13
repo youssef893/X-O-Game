63cfa41e3b7dc5cb643d0911b289d4dd0fa670e4
youssef893 <youssefsayed893@gmail.com>
2020-07-13 17:45:45 +0200

    Create MainActivity.java

---
+++ src/main/java/com/example/tic_tac_toc/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tic_tac_toc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void single(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Single.class);
+        startActivity(intent);
+    }
+
+    public void multi(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Multi.class);
+        startActivity(intent);
+    }
+
+    public void online(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Online.class);
+        startActivity(intent);
+    }
+}
